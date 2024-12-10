0716a651c90b92114006f8c26e42c453ff3cd397
shreyasbangera <shreyasybangera1@gmail.com>
2024-12-10 18:12:23 +0530

    resolve type error in params property of PageProps for noteId

--- frontend/src/app/note/[noteId]/page.tsx
+++ frontend/src/app/note/[noteId]/page.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Tiptap from '@/app/components/TipTap'
 import { fetchNotes } from '@/app/lib/data';
 
-export default async function page({params} : {params: {noteId : string}}) {
-  const noteData = await fetchNotes(params.noteId);
+export default async function page({params} : {params: Promise<{noteId: string}>}) {
+  const noteId = (await params).noteId
+  const noteData = await fetchNotes(noteId);
   return (
     <div className="lg:px-6 pt-16 lg:pt-20">
-    <Tiptap content={noteData?.note || ""} noteId={params.noteId} />
+    <Tiptap content={noteData?.note || ""} noteId={noteId} />
     </div>
   )
 }
